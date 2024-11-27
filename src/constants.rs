de97abd95baecf82e1d86d0f14af3a6eb85da5b6
Illusion <1161524405@qq.com>
2024-11-27 20:49:08 +0800

    add some constant variables

--- src/constants.rs
+++ src/constants.rs
@@ -1,4 +1,14 @@
+use bevy::log::warn;
+
 pub const WINDOW_WIDTH: f32 = 800.0;
 
 // A half of height is eq 256px
 pub const WINDOW_HEIGHT: f32 = 512.0;
+
+pub const GROUND_HEIGHT: f32 = 112.0;
+
+pub const BIRD_SCALE: f32 = 1.2;
+
+pub const BIRD_HEIGHT: f32 = 40.0;
+
+pub const BIRD_SCALED_HEIGHT: f32 = BIRD_SCALE * BIRD_HEIGHT;
