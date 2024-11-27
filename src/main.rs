24cc40129980e31492c417535b58c2053723bcd0
Lucas chen <lucas.chen@gbo-asia.com>
2024-11-27 14:11:06 +0800

    register systems

--- src/main.rs
+++ src/main.rs
@@ -5,8 +5,10 @@ mod plugin;
 mod constants;
 mod setup;
 mod components;
+mod systems;
 
 use setup::setup;
+use systems::{blink_space_bar_text, move_background};
 
 fn main() {
     // Create a bevy app
@@ -14,5 +16,7 @@ fn main() {
         // Customize window size
         .add_plugins(OhMyPlugin)
         .add_systems(Startup, setup)
+        .add_systems(Update, blink_space_bar_text)
+        .add_systems(Update, move_background)
         .run();
 }
