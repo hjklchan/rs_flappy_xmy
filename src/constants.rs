23686f8a0ac691b704fe032e5229bcd33d5d6100
Lucas chen <lucas.chen@gbo-asia.com>
2024-11-28 11:39:33 +0800

    add commonly used constants

--- src/constants.rs
+++ src/constants.rs
@@ -7,6 +7,10 @@ pub const GROUND_HEIGHT: f32 = 112.0;
 
 pub const BIRD_SCALE: f32 = 1.2;
 
+pub const BIRD_WIDTH: f32 = 126.0;
 pub const BIRD_HEIGHT: f32 = 40.0;
-
+pub const BIRD_SCALED_WIDTH: f32 = BIRD_SCALE * BIRD_WIDTH;
 pub const BIRD_SCALED_HEIGHT: f32 = BIRD_SCALE * BIRD_HEIGHT;
+
+pub const PIPE_WIDTH: f32 = 52.0;
+pub const PIPE_HEIGHT: f32 = 32.0;
