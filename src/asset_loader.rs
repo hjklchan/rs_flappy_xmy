c4112fe09dfa0812dd8edd782acff8171b0be860
Lucas chen <lucas.chen@gbo-asia.com>
2024-11-28 13:54:46 +0800

    add and register AssetLoader plugin

---
+++ src/asset_loader.rs
@@ -0,0 +1,28 @@
+use bevy::prelude::*;
+
+use crate::resources::GameAssets;
+
+pub struct AssetLoaderPlugin;
+
+impl Plugin for AssetLoaderPlugin {
+    fn build(&self, app: &mut App) {
+        app.add_systems(PostStartup, load_assets);
+    }
+}
+
+fn load_assets(mut game_assets: ResMut<GameAssets>, asset_server: Res<AssetServer>) {
+    // Load assets
+    let resource = GameAssets {
+        background_texture: asset_server.load("texture/background.png"),
+        ground_texture: asset_server.load("texture/base.png"),
+        game_over_texture: asset_server.load("texture/game-over.png"),
+        space_texture: asset_server.load("texture/space.png"),
+        numbers_texture: asset_server.load("texture/numbers.png"),
+        bird_xmy_texture: asset_server.load("texture/bird-xmy.png"),
+        pipe_texture: asset_server.load("texture/pipe.png"),
+        hit_audio: asset_server.load("audio/hit.ogg"),
+        wing_audio: asset_server.load("audio/wing.ogg"),
+    };
+
+    *game_assets = resource;
+}
